25f9fcea03be11dfc26090586353cbf814462b4a
Nguyen Gia Hao <giahao9899@AIV.T.Shark>
2018-12-06 09:43:44 +0700

    v-1.0.0

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            1024:{
+                items:4,
+                autoplay:false,
+            }
+        }
+    });
+});
